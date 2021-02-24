da2c1938662d59881f8876a031b8bc3ebb70f254
PetteriSetteri <jonne.miettinen@edu.tampere.fi>
2021-02-24 20:37:46 +0200

    fanikauppa

---
+++ fanikauppa/precache-manifest.7be31d272e6e453d8e4de5d6ba88f711.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7e5a137a263748a0bcc825f608f06989",
+    "url": "/fanikauppa/index.html"
+  },
+  {
+    "revision": "b227e1b4f39b89fcfd5d",
+    "url": "/fanikauppa/static/css/main.687f808c.chunk.css"
+  },
+  {
+    "revision": "add44731335462b53280",
+    "url": "/fanikauppa/static/js/2.2966fe03.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/fanikauppa/static/js/2.2966fe03.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b227e1b4f39b89fcfd5d",
+    "url": "/fanikauppa/static/js/main.6ade13bc.chunk.js"
+  },
+  {
+    "revision": "536cbf57df9b1061aef0",
+    "url": "/fanikauppa/static/js/runtime-main.d8c219fb.js"
+  }
+]);
